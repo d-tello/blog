1e0bec5879b1c317c0d25a5f9eda291cfdd14489
Davide Tello <tello.davide@gmail.com>
2021-06-23 11:06:26 +0200

    Add components to the home page

--- sections/Header.jsx
+++ sections/Header.jsx
@@ -7,7 +7,7 @@ const Header = () => {
         <p className="flex items-center space-x-1 text-blue-600">
           <TerminalIcon className="w-8 h-8 flex-shrink-0" />
           <span className="font-bold text-lg tracking-tight whitespace-nowrap">
-            Blog
+            Blog for dev
           </span>
         </p>
       </div>
