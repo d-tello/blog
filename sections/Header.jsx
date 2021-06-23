2a8f9e070747e9190fd35a25be4373b33af3e5d6
Davide Tello <tello.davide@gmail.com>
2021-06-23 11:13:20 +0200

    Logo redirect to home page

--- sections/Header.jsx
+++ sections/Header.jsx
@@ -1,15 +1,10 @@
-import { TerminalIcon } from "@heroicons/react/outline";
+import Logo from "../components/Logo";
 
 const Header = () => {
   return (
     <header className="border-b border-gray-100">
       <div className="container mx-auto px-4 sm:px-6 py-4">
-        <p className="flex items-center space-x-1 text-blue-600">
-          <TerminalIcon className="w-8 h-8 flex-shrink-0" />
-          <span className="font-bold text-lg tracking-tight whitespace-nowrap">
-            Blog for dev
-          </span>
-        </p>
+        <Logo />
       </div>
     </header>
   );
