048d2b53d6216a581b07226e5020a1ff8c86d0c9
Davide Tello <tello.davide@gmail.com>
2021-06-29 15:18:27 +0200

    Add progress bar when loading pages

--- pages/_app.js
+++ pages/_app.js
@@ -1,6 +1,13 @@
 import "../styles/globals.css";
+import "../styles/nprogress.css";
+import NProgress from "nprogress";
+import Router from "next/router";
 import { ThemeProvider } from "next-themes";
 import { Provider as AuthProvider } from "next-auth/client";
+
+Router.events.on("routeChangeStart", () => NProgress.start());
+Router.events.on("routeChangeComplete", () => NProgress.done());
+Router.events.on("routeChangeError", () => NProgress.done());
 
 function MyApp({ Component, pageProps }) {
   return (
